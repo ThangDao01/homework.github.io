6e8250b7c5faa8b0f063f3d10c0035e2ef825ce3
ThangDao01 <thangddth2009001@fpt.edu.vn>
2021-06-02 18:10:22 +0700

    first commit

---
+++ Exercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = new int[3];
+            int max=0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+
+            Console.WriteLine("{0}",max);
+        }
+    }
+}
---
+++ Exercise4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise4_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your phone");
+            string phone = Console.ReadLine();
+            Console.WriteLine("Name : {0} \n Address : {1} \n Phone : {2}",name,address,phone);
+        }
+    }
+}
